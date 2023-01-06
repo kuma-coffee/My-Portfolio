56ad7aabb35a0fff624e6e0732872444869c3df9
kuma-coffee <daniel099d@gmail.com>
2023-01-07 04:01:42 +0700

    major update

---
+++ database/db.js
@@ -0,0 +1,28 @@
+const db = [
+  {
+    link: "",
+    Image: "/img/portfolio-apple.jpeg",
+  },
+  {
+    link: "",
+    Image: "/img/portfolio-stripe.jpeg",
+  },
+  {
+    link: "",
+    Image: "/img/portfolio-stripe.jpeg",
+  },
+  {
+    link: "",
+    Image: "/img/portfolio-apple.jpeg",
+  },
+  {
+    link: "",
+    Image: "/img/portfolio-stripe.jpeg",
+  },
+  {
+    link: "",
+    Image: "/img/portfolio-stripe.jpeg",
+  },
+];
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const db = require("./database/db");
+
+const app = express();
+
+//middleware
+app.use(express.static("assets"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+//view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { items: db });
+});
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is runnning on port 3000`);
+});
